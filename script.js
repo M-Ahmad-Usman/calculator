099abcfaf04342b9e1bd3c283c053a9708b4e910
Muhammad Ahmad <developer.ahmad@proton.me>
2025-07-06 05:51:54 +0500

    Replace p with input to allow user to control cursor.
    Now the use can control the cursor's position. Edit styles and script accomodate change in markup.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import evaluateInfixExpression from "./evaluate_expression.js"
 import { validOperators } from "./evaluate_expression.js"
 
 const previousExpression = document.querySelector(".expression>p");
-const inputOutput = document.querySelector(".output>p");
+const inputOutput = document.querySelector(".input-output>input");
 
 function handleButtonClick(button) {
     if (!(button instanceof HTMLButtonElement)) {
@@ -12,36 +12,36 @@ function handleButtonClick(button) {
     let value = button.textContent;
 
     if (/\d/.test(value) || validOperators.test(value)) {
-        inputOutput.textContent += value;
+        inputOutput.value += value;
     }
     else if (value == "C") {
-        inputOutput.textContent = "";
+        inputOutput.value = "";
         previousExpression.textContent = "";
     }
     else if (value == "CE") {
-        let expression = inputOutput.textContent;
+        let expression = inputOutput.value;
 
         // Check if operator exists in the expression
         if (validOperators.test(expression)) {
             const indexOfLastOperator = expression.split("").findLastIndex(char => validOperators.test(char));
             
             // Remove string after the last operator in the expression.
-            inputOutput.textContent = expression.slice(0, indexOfLastOperator + 1);
+            inputOutput.value = expression.slice(0, indexOfLastOperator + 1);
         }
         // If no operator is found
         else {
             // Remove the whole expression
-            inputOutput.textContent = "";
+            inputOutput.value = "";
         }
     }
     else if (value == "x") {
-        inputOutput.textContent = inputOutput.textContent.slice(0, -1);
+        inputOutput.value = inputOutput.value.slice(0, -1);
     }
     else if (value == "=") {
         let result;
 
         try {
-            result = evaluateInfixExpression(inputOutput.textContent);
+            result = evaluateInfixExpression(inputOutput.value);
         }
         catch (error) {
             console.log(error)
@@ -51,8 +51,8 @@ function handleButtonClick(button) {
             return;
         }
 
-        previousExpression.textContent = inputOutput.textContent;
-        inputOutput.textContent = result;
+        previousExpression.textContent = inputOutput.value;
+        inputOutput.value = result;
     }
 }
 document.querySelector(".buttons").addEventListener("click", (e) => handleButtonClick(e.target));
