933e3d8c2e4d1506ff58255c7ad358eeaad67ff4
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-07-07 10:01:38 -0700

    Change the structure of handleButtonClick.
    To make it multipurpose and more readable.

--- script.js
+++ script.js
@@ -3,56 +3,61 @@ import { validOperators } from "./evaluate_expression.js"
 
 const previousExpression = document.querySelector(".expression>p");
 const inputOutput = document.querySelector(".input-output>input");
+const validInputs = /[-+*/%\.\d()[\]{}]/;
 
-function handleButtonClick(button) {
-    if (!(button instanceof HTMLButtonElement)) {
+function handleInput(input) {
+
+    if (validInputs.test(input)) {
+        inputOutput.value += input;
         return;
     }
 
-    let value = button.textContent;
-
-    if (/\d/.test(value) || validOperators.test(value)) {
-        inputOutput.value += value;
-    }
-    else if (value == "C") {
-        inputOutput.value = "";
-        previousExpression.textContent = "";
-    }
-    else if (value == "CE") {
-        let expression = inputOutput.value;
-
-        // Check if operator exists in the expression
-        if (validOperators.test(expression)) {
-            const indexOfLastOperator = expression.split("").findLastIndex(char => validOperators.test(char));
-            
-            // Remove string after the last operator in the expression.
-            inputOutput.value = expression.slice(0, indexOfLastOperator + 1);
-        }
-        // If no operator is found
-        else {
-            // Remove the whole expression
+    switch (input) {
+        case "C":
             inputOutput.value = "";
-        }
-    }
-    else if (value == "x") {
-        inputOutput.value = inputOutput.value.slice(0, -1);
-    }
-    else if (value == "=") {
-        let result;
+            previousExpression.textContent = "";
+            break;
 
-        try {
-            result = evaluateInfixExpression(inputOutput.value);
-        }
-        catch (error) {
-            console.log(error)
-        }
+        case "x":
+            inputOutput.value = inputOutput.value.slice(0, -1);
+            break;
 
-        if (!result) {
-            return;
-        }
+        case "CE":
+            let expression = inputOutput.value;
 
-        previousExpression.textContent = inputOutput.value;
-        inputOutput.value = result;
+            // Check if operator exists in the expression
+            if (validOperators.test(expression)) {
+                const indexOfLastOperator = expression.split("").findLastIndex(char => validOperators.test(char));
+
+                // Remove string after the last operator in the expression.
+                inputOutput.value = expression.slice(0, indexOfLastOperator + 1);
+            }
+            // If no operator is found
+            else {
+                // Remove the whole expression
+                inputOutput.value = "";
+            }
+
+            break;
+
+        case "=":
+            let result;
+
+            try {
+                result = evaluateInfixExpression(inputOutput.value);
+            }
+            catch (error) {
+                previousExpression.textContent = error.message;
+                inputOutput.value = "";
+                return;
+            }
+
+            previousExpression.textContent = inputOutput.value;
+            inputOutput.value = result;
+
+            break;
     }
 }
-document.querySelector(".buttons").addEventListener("click", (e) => handleButtonClick(e.target));
+
+// Hande Button input
+document.querySelector(".buttons").addEventListener("click", (e) => handleInput(e.target.textContent));
